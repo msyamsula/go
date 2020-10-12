481601bdeec406a1d2132b6dd21f5b1940d7c564
SyamsulAlterra <syamsul@alterra.id>
2020-10-12 10:45:35 +0700

    go init

---
+++ PlayingCards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// variable declaration
+	var card string = "Ace of Spades"
+	fmt.Println(card)
+}
---
+++ hello.go
@@ -0,0 +1,10 @@
+// if you need main function the package shoud be named "main"
+package main
+
+// import statements
+import "fmt"
+
+// "main function"
+func main() {
+	fmt.Println("Hi there!")
+}
